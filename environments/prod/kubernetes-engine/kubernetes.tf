0a4e5eb34b214ed2deab41645d023b20a1f07687
Taisei Nishiumi <westelh@gmail.com>
2025-05-20 23:12:58 +0900

    Upgrade kubernetes module to 1.0.0

--- environments/prod/kubernetes-engine/kubernetes.tf
+++ environments/prod/kubernetes-engine/kubernetes.tf
@@ -1,6 +1,6 @@
 module "kubernetes_engine" {
   source            = "westelh/kubernetes-secret-engine/vault"
-  version           = "0.0.2"
+  version           = "1.0.0"
   description       = "Kubernetes Secret Engine"
   roles             = {}
   extra_annotations = {}
